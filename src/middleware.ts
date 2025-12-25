9eb4793cea80d9e6cc8a4e4390fb03b56dd96e86
Dhiraj <dkskp2005@gmail.com>
2025-12-25 21:44:46 +0530

    auth:protected dashboard

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  const isDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+
+  if (isDashboard && !token) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
