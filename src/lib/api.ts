1b6b3907a2d049bcd09bc5adef2f3822b7ef8f3c
Dhiraj <dkskp2005@gmail.com>
2025-12-25 21:23:24 +0530

    auth interceptor token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
+import { getToken } from "./auth";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
