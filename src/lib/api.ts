6d26f5520b076f32fec62491f354b30bacf681a1
Dhiraj <dkskp2005@gmail.com>
2026-01-05 23:53:38 +0530

    config for ec2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
